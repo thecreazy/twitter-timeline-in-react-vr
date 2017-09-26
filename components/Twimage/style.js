ac45aef4732b529f2031472cf7d787096819f758
Riccardo Canella <riccardo.canella@caffeina.com>
2017-09-26 09:44:26 +0200

    create pages branch for github pages

--- components/Twimage/style.js
+++
@@ -1,8 +0,0 @@
-import {StyleSheet} from 'react-vr';
-
-export default styles = StyleSheet.create({
-    default:{
-        height:'100%',
-        width: '100%'
-    }
-});
--- components/Twtext/style.js
+++
@@ -1,6 +0,0 @@
-import {StyleSheet} from 'react-vr';
-
-export default styles = StyleSheet.create({
-    top:{fontSize: 14, textAlign: 'center', position: 'absolute', top:0},
-    bottom:{fontSize: 14, textAlign: 'center', position: 'absolute', bottom:0, paddingLeft:10, paddingRight:10}
-});
